4c050bedaa846217aef0b514152f1baef17c49ce
Victor Rubiño <105171818+rubinoDev@users.noreply.github.com>
2022-07-21 16:33:30 -0300

    fixing img bug

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import trollFace from "/src/assets/trollFace.png"
+
 export function Header(){
   return(
     <header className="header">
       <div className="header__title-container">
-        <img className="header__trollFace" src="./src/assets/trollFace.png"/>
+        <img className="header__trollFace" src={trollFace}/>
         <h1 className="header__title">Meme generator</h1>
       </div>
       <h4 className="header__h4">React Course - Project 3</h4>
